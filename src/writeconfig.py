de0b66f9b473b8c338bccbb6a6f25bed4bbae0f8
marcel <m.zuur@student.utwente.nl>
2015-01-25 23:49:22 +0100

    Added some small comments to the settings file

--- src/writeconfig.py
+++ src/writeconfig.py
@@ -1,7 +1,7 @@
 import ConfigParser
 
 
-Config = ConfigParser.ConfigParser()
+Config = ConfigParser.ConfigParser(allow_no_value = True)
 # lets create that config file for next time...
 cfgfile = open("settings.ini", 'w')
 
@@ -10,9 +10,11 @@ Config.add_section('Connection')
 Config.set('Connection', 'hostname', "127.0.0.1")
 Config.set('Connection', 'port', 1972)
 Config.add_section('LED')
+Config.set("LED", "; Set the GPIO pin numbering and led flickering frequencies")
 Config.set("LED", "gpio", [13, 15, 11])
 Config.set("LED", "frequencies", [11, 13, 9])
 Config.add_section('Calibration')
+Config.set("Calibration", "; Modify the calibration sequence")
 Config.set("Calibration", "sequences", 6)
 Config.set("Calibration", "interEpochDelay", 0.5)
 Config.set("Calibration", "stimulusDuration", 2.5)
@@ -21,6 +23,7 @@ Config.set("Calibration", "stimulusFullDuration", 2)
 Config.set("Calibration", "interSequenceDelay", 5)
 Config.add_section('SignalProcessing')
 Config.set("SignalProcessing", "fsample", 256)
+Config.set('SignalProcessing', '; SVM for Linear Support Vector Classification or LR for Logistic Regression')
 Config.set("SignalProcessing", "classifier", "SVM")
 Config.set("SignalProcessing", "channels", range(9, 22))
 Config.write(cfgfile)
