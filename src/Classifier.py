b52d68ed717f3cf77d1545d9b41ada79789fdb77
marcel <m.zuur@student.utwente.nl>
2015-01-25 14:22:05 +0100

    Combined the SVM and Logistic regression classifiers in one file

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,119 +1,110 @@
-import StringIO
-import numpy as np
-import scipy
-from sklearn import svm, preprocessing
-from sklearn.grid_search import GridSearchCV
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.metrics import metrics
-from network import preproc
 import pickle
 
+import numpy as np
+import scipy
+import sklearn.svm
+from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 
-class Classifier():
+from network import preproc
 
-    def __init__(self, fsample=250.0, channel_idxs=np.arange(0,10)):
-        n_cv_folds = 3
-        c_grid = [0.001, 0.01, 1, 10]
-        model = LogisticRegression(C = 1.0, penalty = 'l1',  random_state=0)
-        #model = SGDClassifier(loss='log', penalty='l1', random_state=0)
-        #model = svm.SVC(kernel='linear', verbose=False, max_iter=1000000)
-        #self.clf = GridSearchCV(model, param_grid={'C': c_grid}, cv=n_cv_folds,
-        #                   scoring = 'accuracy')
-        self.clf = svm.LinearSVC()
-        self.events = None
+
+class Classifier(object):
+    def __init__(self, fsample=250.0, channel_idxs=np.arange(9, 22)):
+        self.clf = None
         self.fsample = fsample
-        self.optimal_model = self.clf
         self.channel_idxs = channel_idxs
 
-    def _preprocess(self, data, events=None):
-        #data = preproc.detrend(data)
-        data, badch = preproc.badchannelremoval(data)
-        #for i in range(len(data)):
-        #    data[i]=(preproc.spatialfilter(data[i]))
-
-        #data = preproc.fouriertransform(data, self.fsample)
-        data, badch = preproc.badchannelremoval(data,range(23,47,1))
-        data, badch = preproc.badchannelremoval(data,range(1,8,1))
-
-        data = preproc.spectralfilter(data,[1,5,80,85], 250.0) #TODO: focus on the pre-defined frequencies.
-        #if events is not None:
-        #    data, events, badtrials = preproc.badtrailremoval(data, events) #FIXME: check outlierdetection iterValue
-        #    self.events = events
-        return data, events
-
-    def _convert_data(self, data):
-        #spectrum = preproc.spectrum(data, self.fsample)
-        #spectrum = np.array(spectrum)
-        #boolean_indexes = spectrum > 6
-        #indexes = np.array(np.where(boolean_indexes)[0]).astype(np.int32)
-
+    def convert_data(self, data):
         X = np.array(data, dtype=np.float32)
-        #X = np.ascontiguousarray(X[:, indexes, :]) #FIXME: focus on the pre-defined frequencies?
-        #X = np.ascontiguousarray(X[:, :, self.channel_idxs])
-        X.shape=(X.shape[0], X.shape[1] * X.shape[2])
+        freqs, X = scipy.signal.welch(X, fs=self.fsample, axis=1, scaling='spectrum', detrend='linear',
+                                      window='hanning', )
+        freqs_idxs = np.logical_and(freqs > 1, freqs < 80)
+        X = np.ascontiguousarray(X[:, freqs_idxs, :])
+        X = np.ascontiguousarray(X[:, :, self.channel_idxs])
+        X.shape = (X.shape[0], X.shape[1] * X.shape[2])
         return X
 
-    def _convert_events(self, events):
+    def convert_events(self, events):
         y = np.zeros(len(events), dtype=np.int32)
         for i in range(len(events)):
             y[i] = events[i].value
         return y
 
-    def train(self, data, events): #TODO: welch window
-        data, events = self._preprocess(data, events)
-        X = self._convert_data(data)
-        y = self._convert_events(events)
-        self.clf.fit(X,y)
-        #print(self.clf.best_params_, self.clf.best_score_)
-        # print self.clf.fit
-        self.optimal_model = self.clf
-        #   return self.clf.best_score_
+    def train(self, data, events):
+        X = self.convert_data(data)
+        Y = self.convert_events(events)
+        self.clf.fit(X, Y)
 
     def predict(self, data):
-        data, events = self._preprocess(data)
-        X = self._convert_data(data)
-        return self.optimal_model.predict(X)
+        X = self.convert_data(data)
+        return self.clf.predict(X)
 
     def plot_model(self, data, classifier):
         from matplotlib import pyplot as plt
+
         for i in range(4):
-            ax=plt.subplot(2, 2, i)
+            ax = plt.subplot(2, 2, i)
             plt.gray()
             spectrum = preproc.spectrum(data, classifier.fsample)
             spectrum = np.array(spectrum).astype(np.int32)
-            #indexes = np.array(np.where(np.logical_and(spectrum > 9, spectrum < 29))[0]).astype(np.int32)
+            # indexes = np.array(np.where(np.logical_and(spectrum > 9, spectrum < 29))[0]).astype(np.int32)
             plt.imshow(coefs_matrix[i].T, interpolation='nearest')
         plt.show()
 
+    def validate(self,data,events):
+        predictions = classifier.predict(data)
+        print(predictions)
+        y = classifier.convert_events(events)
+        print(y)
+        assert len(predictions) == len(y)
+        print("test accuracy: ", np.sum(predictions == y) / float(y.shape[0]))
+
+class LRClassifier(Classifier):
+    def __init__(self, fsample=250.0, channel_idxs=np.arange(9, 22)):
+        super(LRClassifier, self).__init__(fsample, channel_idxs)
+        n_cv_folds = 3
+        c_grid = [0.001, 0.01, 1, 10]
+        model = LogisticRegression(C=1.0, penalty='l1', random_state=0)
+        self.clf = GridSearchCV(model, param_grid={'C': c_grid}, cv=n_cv_folds,
+                                scoring='accuracy')
+        self.events = None
+
+
+class SVMClassifier(Classifier):
+    def __init__(self, fsample=250.0, channel_idxs=np.arange(9, 22)):
+        super(SVMClassifier, self).__init__(fsample, channel_idxs)
+        self.clf = sklearn.svm.LinearSVC()
+
 
 if __name__ == '__main__':
-    with open("subject_data_marcel1", "rb") as f:
+    with open("subject_data_marcel2101_2", "rb") as f:
         data_tuple = np.load(f)
         data = data_tuple['data']
         events = data_tuple['events']
 
-    for i in range(1):
-        print(i)
-        classifier = Classifier()
-        classifier.train(data[0:len(data)/2], events[0:len(data)/2])
+    #Test the SVM classifier
+    classifier = SVMClassifier()
+    #classifier.train(data[0:len(data) / 2], events[0:len(data) / 2])
+    classifier.train(data[0:len(data)-30],events[0:len(data)-30])
+    classifier.validate(data[len(data)-30:len(data)],events[len(data)-30:len(data)])
+    #predictions = classifier.predict(data[len(data) / 2:len(data)])
 
-    predictions = classifier.predict(data[len(data)/2:len(data)])
-    print(predictions)
-
-    #training accuracy
-    y = classifier._convert_events(events[len(data)/2:len(data)])
-    print(y)
-    assert len(predictions)==len(y)
-    print("test accuracy: ", np.sum(predictions==y)/float(y.shape[0]))
+    #Test the logistic regression classifier
+    classifier = LRClassifier()
+    #   classifier.train(data[0:len(data) / 2], events[0:len(data) / 2])
+    #predictions = classifier.predict(data[len(data) / 2:len(data)])
+    classifier.train(data[0:len(data)-30],events[0:len(data)-30])
+    classifier.validate(data[len(data)-30:len(data)],events[len(data)-30:len(data)])
 
     data_array = np.array(data)
 
     with open("subject2_classifier", "wb") as f:
-        pickle.dump({"model":classifier.optimal_model}, f)
+        pickle.dump({"model": classifier.clf}, f)
 
     with open("subject2_classifier", "rb") as f:
         model = np.load(f)['model']
 
-    assert (model.coef_==classifier.optimal_model.coef_).all()
-    coefs_matrix=model.coef_.reshape(4, model.coef_.shape[1]/10, 10)
+    assert (model.coef_ == classifier.optimal_model.coef_).all()
+    coefs_matrix = model.coef_.reshape(4, model.coef_.shape[1] / 10, 10)
     classifier.plot_model(data, classifier)
